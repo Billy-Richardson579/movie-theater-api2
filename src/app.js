d2498bb675bbd58b2fbfc8c40ef1c335459de63d
Billy Richardson <billy7373731@outlook.com>
2023-05-12 21:39:31 +0100

    user.js done app.js updated

--- src/app.js
+++ src/app.js
@@ -1,14 +1,13 @@
 const express = require('express');
 const app = express();
-const routerUser = require('./routers/user')
-const routerShow = require('./routers/show'); 
+const routerUser = require('./routers/user');
+const routerShow = require('./routers/show');
 
 // Middleware
 app.use(express.json());
 
 // Routes
-app.use('/user', routerShow);
+app.use('/users', routerUser);
+app.use('/shows', routerShow);
 
-app.use('/show', routerUser);
-
-  module.exports = {app,routerShow,routerUser};
+module.exports = { app, routerUser, routerShow };
--- src/routers/user.js
+++ src/routers/user.js
@@ -3,41 +3,60 @@ const {User }= require("../../models/index")
 const router = Router()
 const {Show} = require('../../models/index')
 
-
 router.get("/", async (req, res, next) => {
-    const user = await User.findAll();
-    res.json(user);
-    next();
-});
-
-router.get('/:id', async (req, res,next) => {
-    const chosenId = req.params.id;
+    try {
+      const users = await User.findAll();
+      res.json(users);
+    } catch (error) {
+      next(error);
+    }
+  });
   
-
-      const userById = await User.findByPk(chosenId,{include: Show});
-  
-      if (userById) {
-        res.json(userById);
-        next()
+  // Get one user by ID
+  router.get("/:id", async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const user = await User.findByPk(id);
+      if (user) {
+        res.json(user);
       } else {
-        // If Show is not found, send a 404 response
-        res.status(404).json({ error: 'Show not found' });
+        res.status(404).json({ error: "User not found" });
       }
-    });
-    router.get("/:id/show",async(req,res)=>{
-        const {id }= req.params
-        const user = await User.findByPk(id,{include:Show});
+    } catch (error) {
+      next(error);
+    }
+  });
+  
+  // Get all shows watched by a user
+  router.get("/:id/shows", async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const user = await User.findByPk(id, { include: Show });
+      if (user) {
         res.json(user.Shows);
-
-    })
-
-    router.put("/:id/show/:showId",async(req,res)=>{
-        const {id,showId} = req.params
-        const user = awaitUser.findByPk(id,{include: Show});
-        const userShow = await Show.findByPk(showId);
-        await user.addShows(userShow);
+      } else {
+        res.status(404).json({ error: "User not found" });
+      }
+    } catch (error) {
+      next(error);
+    }
+  });
+  
+  // Update and add a show if a user has watched it
+  router.put("/:id/shows/:showId", async (req, res, next) => {
+    try {
+      const { id, showId } = req.params;
+      const user = await User.findByPk(id);
+      const show = await Show.findByPk(showId);
+      if (user && show) {
+        await user.addShow(show);
         res.status(200).send("Show updated successfully");
-
-    })
+      } else {
+        res.status(404).json({ error: "User or Show not found" });
+      }
+    } catch (error) {
+      next(error);
+    }
+  });
       
       module.exports = router;
